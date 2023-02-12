a2ffda2e516ae67a32c91098109f5a9361caa515
Logan Gyaltsen <gyaljav@gmail.com>
2023-02-12 12:36:09 -0500

    modified

--- codea/urls.py
+++ codea/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
+
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# urlpatterns = [
+#     path("", views.nangchen, name="nangchen"),
+#     path("_base/", views._base, name="base"),
+#     path("about/", views.about, name="about"),
+#     path("contact/", views.contact, name="contact"),
+#     path("history/", views.history, name="history"),
+# ] + static(settings.STATIC_URL, ducument_root=settings.STATIC_ROOT)
 
 urlpatterns = [
     path("", views.nangchen, name="nangchen"),
@@ -9,4 +18,4 @@ urlpatterns = [
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
     path("history/", views.history, name="history"),
-] + static(settings.STATIC_URL, ducument_root=settings.STATIC_ROOT)
+]
