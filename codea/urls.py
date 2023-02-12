076eb1da773e9620b810e175dea06cc14cea73fc
Logan Gyaltsen <gyaljav@gmail.com>
2023-02-12 10:20:15 -0500

    html

--- codea/urls.py
+++ codea/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.nangchen, name="nangchen"),
+    path("about/", views.about, name="about"),
 ]
--- codea/views.py
+++ codea/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def nangchen(request):
     return render(request, "nangchen.html")
+
+
+def about(request):
+    return render(request, "about.html")
