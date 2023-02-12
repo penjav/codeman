4b99ef21d17519764afbd57b7009c081a7c8ca8e
Logan Gyaltsen <gyaljav@gmail.com>
2023-02-12 12:22:31 -0500

    collect static

--- codea/urls.py
+++ codea/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.nangchen, name="nangchen"),
@@ -7,4 +9,4 @@ urlpatterns = [
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
     path("history/", views.history, name="history"),
-]
+] + static(settings.STATIC_URL, ducument_root=settings.STATIC_ROOT)
