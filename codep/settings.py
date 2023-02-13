c379172c6c7e41653ed6806c578b16778f69ef54
Logan Gyaltsen <gyaljav@gmail.com>
2023-02-12 19:57:32 -0500

    raise key

--- codep/settings.py
+++ codep/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+
+# from environs import Env
+import environs
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +24,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
+# env = Env()
+# env.read_env()
+# secret = Env["SECRET"]
+
+env = environs.Env()
+
+# read the .env file
+environs.Env.read_env()
+
+SECRET_KEY = env("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
