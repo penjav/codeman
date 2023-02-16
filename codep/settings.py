f436a0739aee7bb18d54f3b26f830fa0580fc315
Logan Gyaltsen <gyaljav@gmail.com>
2023-02-16 12:26:10 -0500

    environs

--- codep/settings.py
+++ codep/settings.py
@@ -13,17 +13,21 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 from environs import Env
+import environs
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-env = Env()
+env = environs.Env()
 env.read_env()  # read .env file if it exist
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Override in .env for local development
 DEBUG = env.bool("DEBUG", default=False)
