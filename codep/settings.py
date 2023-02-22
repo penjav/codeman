93938a68521b61a70c68429554fc354ae2295b75
Logan Gyaltsen <gyaljav@gmail.com>
2023-02-21 19:08:24 -0500

    message

--- codep/settings.py
+++ codep/settings.py
@@ -44,6 +44,7 @@ SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 ALLOWED_HOSTS = [
     "www.nangchen25.com",
     "nangchen25.com",
